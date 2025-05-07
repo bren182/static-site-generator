d4fdabb5f9dcf83030fc2505e8a5899d81eb6b76
bren182 <drummer.bren@gmail.com>
2025-05-07 14:32:12 +0200

    extra test for markdown images

--- src/tests/test_extract_markdown_images.py
+++ src/tests/test_extract_markdown_images.py
@@ -5,3 +5,9 @@ class TestExtractMarkdownImages(unittest.TestCase):
     def test_can_extract_image_markdown(self):
         result = extract_markdown_images("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)")
         self.assertEqual(result,[('rick roll','https://i.imgur.com/aKaOqIh.gif'), ('obi wan','https://i.imgur.com/fJRm4Vk.jpeg')])
+
+    def test_extract_markdown_images2(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
